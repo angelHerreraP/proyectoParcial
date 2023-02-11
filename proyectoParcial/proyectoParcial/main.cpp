a2e4e69095de31ce5219df4fe405f612814c7c93
Angel Perez <angelhp@Angels-MacBook-Air.local>
2023-02-11 16:11:37 -0600

    Documento

---
+++ proyectoParcial/proyectoParcial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  proyectoParcial
+//
+//  Created by Angel Perez on 11/02/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
